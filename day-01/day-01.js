f09bf6101594e053243b91f5dc54d6ce9f593a22
meow56 <spinattacktotheface@gmail.com>
2022-11-30 23:17:50 -0600

    Day 1-2
    Easy as pie. I predicted we would need the separate calorie values, but I was wrong.
    
    I like my max-3 solution, it feels elegant.

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -2,17 +2,27 @@
 
 function day1(input) {
 	let splitLines = input.split("\n");
-	let elves = [[]];
 	let totals = [0];
 	for(let i = 0; i < splitLines.length; i++) {
 		if(splitLines[i] === "") {
-			elves.push([0]);
 			totals.push(0);
 		} else {
-			elves[elves.length - 1].push(splitLines[i]);
-			totals[elves.length - 1] += parseInt(splitLines[i], 10);
+			totals[totals.length - 1] += parseInt(splitLines[i], 10);
 		}
 	}
-	let maxVal = Math.max(...totals);
-	updateCaption(`The max calorie amount is ${maxVal}.`);
+	let maxVals = totals.reduce(function(acc, val) {
+		if(val > acc[0]) {
+			return [val, acc[0], acc[1]];
+		} else if(val > acc[1]) {
+			return [acc[0], val, acc[1]];
+		} else if(val > acc[2]) {
+			return [acc[0], acc[1], val];
+		}
+		return acc;
+	}, [0, 0, 0]);
+	let maxSum = maxVals.reduce(function(acc, val) {
+		return acc + val;
+	}, 0);
+	updateCaption(`The max calorie amount is ${maxVals[0]}.`);
+	updateCaption(`The sum of the max calorie amounts is ${maxSum}.`);
 }
