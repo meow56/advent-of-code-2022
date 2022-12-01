c0b73f6be627b6431e097bb504fcbbc591050ba4
meow56 <spinattacktotheface@gmail.com>
2022-11-30 21:53:17 -0600

    Better function calls
    I can finally get rid of that horrible switch statement. Isn't this so much nicer?
    
    I feel like I'm gonna get screwed over because of some weird interaction with window, but shh!!
    
    Also I moved devMode to the top just because it seems like a more reasonable place to put it.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const devMode = false;
+
 window.onload = function() {
 	let date = new Date();
 	date.setUTCHours(date.getUTCHours() - 5); // Time zones :(
@@ -12,89 +14,7 @@ function handleFiles() {
 	clearText();
 	clearCaption();
 	const input = this.files[0];
-	input.text().then(text => detDay(text));
-
-	function detDay(text) {
-		const dayInput = document.getElementById("dayNum");
-		const dayNum = dayInput.value;
-		switch(dayNum) { // hmm.
-			case "1":
-				day1(text);
-				break;
-			case "2":
-				day2(text);
-				break;
-			case "3":
-				day3(text);
-				break;
-			case "4":
-				day4(text);
-				break;
-			case "5":
-				day5(text);
-				break;
-			case "6":
-				day6(text);
-				break;
-			case "7":
-				day7(text);
-				break;
-			case "8":
-				day8(text);
-				break;
-			case "9":
-				day9(text);
-				break;
-			case "10":
-				day10(text);
-				break;
-			case "11":
-				day11(text);
-				break;
-			case "12":
-				day12(text);
-				break;
-			case "13":
-				day13(text);
-				break;
-			case "14":
-				day14(text);
-				break;
-			case "15":
-				day15(text);
-				break;
-			case "16":
-				day16(text);
-				break;
-			case "17":
-				day17(text);
-				break;
-			case "18":
-				day18(text);
-				break;
-			case "19":
-				day19(text);
-				break;
-			case "20":
-				day20(text);
-				break;
-			case "21":
-				day21(text);
-				break;
-			case "22":
-				day22(text);
-				break;
-			case "23":
-				day23(text);
-				break;
-			case "24":
-				day24(text);
-				break;
-			case "25":
-				day25(text);
-				break;
-		}
-	}
+	input.text().then(text => window["day" + document.getElementById("dayNum").value](text));
 }
 
 function displayText(text = "") {
@@ -142,5 +62,3 @@ function assignButton(callback, text) {
 	buttons.appendChild(NEW_BUTTON);
 	return NEW_BUTTON;
 }
-
-const devMode = false;
