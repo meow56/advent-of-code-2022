3aaf24623215d1cb61431fedf51c508b885e15a1
meow56 <spinattacktotheface@gmail.com>
2022-11-30 23:32:50 -0600

    Day 1-2.1
    This is more general and compresses the else if into a single line. I'd say that's even more elegant, no?
    
    On the other hand, from a performance standpoint it's probably overkill to use whatever sort is used in Array.sort(). This is one of the funny cases where Bubble sort would be faster. I don't really care to optimize this though, so it's written here instead of actually implemented.

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -11,18 +11,15 @@ function day1(input) {
 		}
 	}
 	let maxVals = totals.reduce(function(acc, val) {
+		// acc is always sorted in ascending order, so the first element is the smallest.
 		if(val > acc[0]) {
-			return [val, acc[0], acc[1]];
-		} else if(val > acc[1]) {
-			return [acc[0], val, acc[1]];
-		} else if(val > acc[2]) {
-			return [acc[0], acc[1], val];
+			return [...acc.slice(1), val].sort((a, b) => a - b);
 		}
 		return acc;
 	}, [0, 0, 0]);
 	let maxSum = maxVals.reduce(function(acc, val) {
 		return acc + val;
 	}, 0);
-	updateCaption(`The max calorie amount is ${maxVals[0]}.`);
+	updateCaption(`The max calorie amount is ${maxVals[maxVals.length - 1]}.`);
 	updateCaption(`The sum of the max calorie amounts is ${maxSum}.`);
 }
