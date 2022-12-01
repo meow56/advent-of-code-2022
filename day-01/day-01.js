8b3da05eec025bc6cd86878f32ecdf3e346454d7
meow56 <spinattacktotheface@gmail.com>
2022-11-30 23:10:45 -0600

    Day 1-1
    It seems the date thing works, that's nice.
    
    Anyways, starting out difficult as always.

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -1,8 +1,18 @@
 "use strict";
 
 function day1(input) {
-	const FILE_REGEX = /./g;
-	let entry;
-	while(entry = FILE_REGEX.exec(input)) {
+	let splitLines = input.split("\n");
+	let elves = [[]];
+	let totals = [0];
+	for(let i = 0; i < splitLines.length; i++) {
+		if(splitLines[i] === "") {
+			elves.push([0]);
+			totals.push(0);
+		} else {
+			elves[elves.length - 1].push(splitLines[i]);
+			totals[elves.length - 1] += parseInt(splitLines[i], 10);
+		}
 	}
+	let maxVal = Math.max(...totals);
+	updateCaption(`The max calorie amount is ${maxVal}.`);
 }
