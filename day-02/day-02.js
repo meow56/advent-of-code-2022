0b1519a5ccc292627c840232425756b1c455abfd
meow56 <spinattacktotheface@gmail.com>
2022-12-01 23:20:33 -0600

    Day 2-2
    Ha, I guessed this twist this time. Only trouble I had this time was mixing up who was who (I thought the first column was me). Also keeping an array of round scores is completely unnecessary.

--- day-02/day-02.js
+++ day-02/day-02.js
@@ -17,10 +17,24 @@ function day2(input) {
 	outcomes.set("CX", 7); // Scissors v Rock
 	outcomes.set("CY", 2); // Scissors v Paper
 	outcomes.set("CZ", 6); // Scissors v Scissors
+	const realOutcomes = new Map();
+	realOutcomes.set("AY", 4); // Rock     v Rock
+	realOutcomes.set("AZ", 8); // Rock     v Paper
+	realOutcomes.set("AX", 3); // Rock     v Scissors
+	realOutcomes.set("BX", 1); // Paper    v Rock
+	realOutcomes.set("BY", 5); // Paper    v Paper
+	realOutcomes.set("BZ", 9); // Paper    v Scissors
+	realOutcomes.set("CZ", 7); // Scissors v Rock
+	realOutcomes.set("CX", 2); // Scissors v Paper
+	realOutcomes.set("CY", 6); // Scissors v Scissors
 	let scores = [];
+	let realScores = [];
 	for(let i = 0; i < rounds.length; i++) {
 		scores[i] = outcomes.get(rounds[i][0] + rounds[i][1]);
+		realScores[i] = realOutcomes.get(rounds[i][0] + rounds[i][1]);
 	}
 	let totalScore = scores.reduce((acc, val) => acc + val, 0);
+	let trueScore = realScores.reduce((acc, val) => acc + val, 0);
 	updateCaption(`The total score is ${totalScore}.`);
+	updateCaption(`The REAL total score is ${trueScore}.`);
 }
