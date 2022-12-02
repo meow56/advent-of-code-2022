56a5a9b928f52fc305e21fc5d0e4b8768d827fd7
meow56 <spinattacktotheface@gmail.com>
2022-12-01 23:39:02 -0600

    Day 2-2.1
    Added a table showing what the strategy guide might look like unencrypted. Makes it run notably slower, taking somewhere around a second now?
    
    Horrible formatting in the for loop displayText btw.

--- day-02/day-02.js
+++ day-02/day-02.js
@@ -29,12 +29,27 @@ function day2(input) {
 	realOutcomes.set("CY", 6); // Scissors v Scissors
 	let scores = [];
 	let realScores = [];
+	const moveNames = new Map();
+	moveNames.set("A", "Rock".padEnd(26));
+	moveNames.set("B", "Paper".padEnd(26));
+	moveNames.set("C", "Scissors".padEnd(26));
+	const outcomeNames = new Map();
+	outcomeNames.set("X", "Loss".padEnd(25));
+	outcomeNames.set("Y", "Draw".padEnd(25));
+	outcomeNames.set("Z", "Win".padEnd(25));
+	displayText(`My RPS Super-Secret Strategy Guide!! [NO PEEKING] [KEEP OUT]`);
+	displayText(`[SERIOUSLY DON'T LOOK I'LL CALL MOM]`);
+	displayText();
+	displayText(`On round...    Your opponent plays...    And the outcome is...    So you score...`);
 	for(let i = 0; i < rounds.length; i++) {
 		scores[i] = outcomes.get(rounds[i][0] + rounds[i][1]);
 		realScores[i] = realOutcomes.get(rounds[i][0] + rounds[i][1]);
+		displayText(`${(i + 1).toString().padStart(11)}    ${moveNames.get(rounds[i][0])}${outcomeNames.get(rounds[i][1])}${realScores[i].toString()}`);
 	}
 	let totalScore = scores.reduce((acc, val) => acc + val, 0);
 	let trueScore = realScores.reduce((acc, val) => acc + val, 0);
 	updateCaption(`The total score is ${totalScore}.`);
 	updateCaption(`The REAL total score is ${trueScore}.`);
+	updateCaption(`The strategy guide is shown in a table.`);
+	updateCaption(`The columns are, from left to right: the round number, the opponent's play, the outcome of the match, and the score of the round.`);
 }
