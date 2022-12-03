efa005c00343c17af256cc4de309da295501cb08
meow56 <spinattacktotheface@gmail.com>
2022-12-02 23:19:02 -0600

    Day 3-1
    Foiled again by the fact that there are apparently like 6 random characters between "Z" and "a". Sad.
    
    Also it seems horribly inefficient to make a regex for every rucksack, don't do that.

--- day-03/day-03.js
+++ day-03/day-03.js
@@ -1,8 +1,21 @@
 "use strict";
 
 function day3(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /[a-zA-Z]+/g;
+	let rucksacks = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
+		rucksacks.push([entry[0].slice(0, entry[0].length / 2), entry[0].slice(entry[0].length / 2)]);
 	}
+	let total = 0;
+	rucksacks.forEach(function(rucksack) {
+		let regex = new RegExp(`[${rucksack[0]}]`);
+		let result = rucksack[1].match(regex)[0];
+		if(result.charCodeAt() >= "a".charCodeAt()) {
+			total += result.charCodeAt() - "a".charCodeAt() + 1;
+		} else {
+			total += result.charCodeAt() - "A".charCodeAt() + 27;
+		}
+	});
+	displayCaption(`The total score is ${total}`);
 }
