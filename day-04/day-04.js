8e2a001101903297fa851aebdc1aeed4aa87336f
meow56 <spinattacktotheface@gmail.com>
2022-12-03 23:08:02 -0600

    Day 4-1
    Not so hard. I bet the second problem is going to be "how many overlap partially?"
    
    Also I forgot to convert them to numbers initially so I got the wrong answer. Unary plus saves the day again.

--- day-04/day-04.js
+++ day-04/day-04.js
@@ -1,8 +1,17 @@
 "use strict";
 
 function day4(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /(\d+)-(\d+),(\d+)-(\d+)/g;
+	let pairs = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
+		pairs.push([[+entry[1], +entry[2]], [+entry[3], +entry[4]]]);
 	}
+	let encapsulated = 0;
+	for(const pair of pairs) {
+		if((pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) || (pair[1][0] >= pair[0][0] && pair[1][1] <= pair[0][1])) {
+			encapsulated++;
+		}
+	}
+	displayCaption(`The number of encapsulated pairs is ${encapsulated}`);
 }
