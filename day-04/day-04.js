b7fe0ac80c23896c2aba11fe8a48f3cbee793b88
meow56 <spinattacktotheface@gmail.com>
2022-12-03 23:12:13 -0600

    Day 4-2
    Called it!
    
    Thankfully I know how overlapping ranges work.
    
    Now onto the visualization...

--- day-04/day-04.js
+++ day-04/day-04.js
@@ -8,10 +8,15 @@ function day4(input) {
 		pairs.push([[+entry[1], +entry[2]], [+entry[3], +entry[4]]]);
 	}
 	let encapsulated = 0;
+	let overlapped = 0;
 	for(const pair of pairs) {
 		if((pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) || (pair[1][0] >= pair[0][0] && pair[1][1] <= pair[0][1])) {
 			encapsulated++;
+			overlapped++;
+		} else if((pair[0][0] <= pair[1][1] && pair[0][1] >= pair[1][0]) || (pair[1][0] <= pair[0][1] && pair[1][1] >= pair[0][0])) {
+			overlapped++;
 		}
 	}
 	displayCaption(`The number of encapsulated pairs is ${encapsulated}`);
+	displayCaption(`The number of overlapped pairs is ${overlapped}`);
 }
