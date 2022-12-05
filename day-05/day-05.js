8fb948cdc1a68dadcfd7e293b48f557071f2f139
meow56 <spinattacktotheface@gmail.com>
2022-12-05 00:17:24 -0600

    Day 5-2.1
    Visual done! I really like this one. There's interactivity if you want to scroll through 501 moves. (Why are there 501 moves and not 500 moves? Sad.)

--- day-05/day-05.js
+++ day-05/day-05.js
@@ -13,15 +13,32 @@ function day5(input) {
 			}
 		}
 	}
-	console.log(stacks);
 	let stacks9001 = [];
 	for(let i = 0; i < stacks.length; i++) {
 		stacks9001.push(stacks[i].slice());
 	}
 	const MOVE_REGEX = /move (\d+) from (\d) to (\d)/g;
+	let stacksStates = [[]];
+	let stacks9001States = [[]];
+	for(let i = 0; i < stacks.length; i++) {
+		stacksStates[0].push(stacks[i].slice());
+	}
+	for(let i = 0; i < stacks9001.length; i++) {
+		stacks9001States[0].push(stacks9001[i].slice());
+	}
+	let moves = [[]];
 	while(entry = MOVE_REGEX.exec(input)) {
+		moves.push([+entry[1], +entry[2], +entry[3]]);
 		move(+entry[1], +entry[2] - 1, +entry[3] - 1);
 		move9001(+entry[1], +entry[2] - 1, +entry[3] - 1);
+		stacksStates.push([]);
+		for(let i = 0; i < stacks.length; i++) {
+			stacksStates[stacksStates.length - 1].push(stacks[i].slice());
+		}
+		stacks9001States.push([]);
+		for(let i = 0; i < stacks9001.length; i++) {
+			stacks9001States[stacks9001States.length - 1].push(stacks9001[i].slice());
+		}
 	}
 
 	function move(number, source, destination) {
@@ -48,4 +65,79 @@ function day5(input) {
 	}
 	displayCaption(`The crates at the top are ${part1Answer}.`);
 	displayCaption(`The (9001) crates at the top are ${part2Answer}.`);
+	displayCaption(`A visual of the stacks is displayed.`);
+	displayCaption(`Use the previous and next buttons to move crates, and flip to change between part 1 and 2. Or skip to the end or return to the start with the respective buttons.`);
+
+	function buttonClosure() {
+		const STACK_PRE = assignBlock("stacks");
+		const INFO_PRE = assignBlock("info");
+		const privateStacksStates = stacksStates;
+		const privateStacks9001States = stacks9001States;
+		const privateMoves = moves;
+		const TOTAL_MOVES = moves.length;
+		let moveNum = 0;
+		let is9001 = false;
+		function displayStacks(theStacks) {
+			STACK_PRE.clearText();
+			INFO_PRE.clearText();
+			let tallestStack = theStacks.reduce(function(acc, val) {
+				return Math.max(acc, val.length);
+			}, 0);
+			for(let i = tallestStack - 1; i >= 0; i--) {
+				let finalString = "";
+				for(let j = 0; j < theStacks.length; j++) {
+					if(theStacks[j][i]) {
+						finalString += `[${theStacks[j][i]}] `;
+					} else {
+						finalString += `    `;
+					}
+				}
+				STACK_PRE.displayText(finalString);
+			}
+			STACK_PRE.displayText(` 1   2   3   4   5   6   7   8   9`);
+			INFO_PRE.displayText(`CrateMover ${is9001 ? "9001" : "9000"}`);
+			if(moveNum !== 0) {
+				INFO_PRE.displayText(`Move ${moveNum}: ${moves[moveNum][0]} from ${moves[moveNum][1]} to ${moves[moveNum][2]}.`);
+			} else {
+				INFO_PRE.displayText(`Initial state.`);
+			}
+		}
+		function decreaseMoveNum() {
+			if(moveNum !== 0) {
+				moveNum--;
+				displayStacks(is9001 ? privateStacks9001States[moveNum] : privateStacksStates[moveNum]);
+			}
+		}
+		function increaseMoveNum() {
+			if(moveNum !== TOTAL_MOVES - 1) {
+				moveNum++;
+				displayStacks(is9001 ? privateStacks9001States[moveNum] : privateStacksStates[moveNum]);
+			}
+		}
+		function flip9001() {
+			is9001 = !is9001;
+			displayStacks(is9001 ? privateStacks9001States[moveNum] : privateStacksStates[moveNum]);
+		}
+		function toEnd() {
+			if(moveNum !== TOTAL_MOVES - 1) {
+				moveNum = TOTAL_MOVES - 1;
+				displayStacks(is9001 ? privateStacks9001States[moveNum] : privateStacksStates[moveNum]);
+			}
+		}
+		function toBeginning() {
+			if(moveNum !== 0) {
+				moveNum = 0;
+				displayStacks(is9001 ? privateStacks9001States[moveNum] : privateStacksStates[moveNum]);
+			}
+		}
+		displayStacks(privateStacksStates[moveNum]);
+		return [decreaseMoveNum, increaseMoveNum, flip9001, toEnd, toBeginning];
+	}
+
+	let [button1F, button2F, button3F, button4F, button5F] = buttonClosure();
+	let button1 = assignButton(button1F, "Previous Move");
+	let button2 = assignButton(button2F, "Next Move");
+	let button3 = assignButton(button3F, "Flip 9000/9001");
+	let button4 = assignButton(button4F, "To End");
+	let button5 = assignButton(button5F, "To Beginning");
 }
