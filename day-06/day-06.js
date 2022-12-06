8ed436dd2e1b725082ce1401c8720e588d6ce59b
meow56 <spinattacktotheface@gmail.com>
2022-12-05 23:18:56 -0600

    Day 6-1
    Oof. I feel like I spent a lot of time on this one. Mostly because I kept trying to find a built in unique array function. Guess I should just use sets for that...

--- day-06/day-06.js
+++ day-06/day-06.js
@@ -1,8 +1,19 @@
 "use strict";
 
 function day6(input) {
-	const FILE_REGEX = /./g;
-	let entry;
-	while(entry = FILE_REGEX.exec(input)) {
+	let currWindow = [input[0], input[1], input[2], input[3]];
+	function isRepeating() {
+		return currWindow[0] === currWindow[1] || currWindow[0] === currWindow[2] || currWindow[0] === currWindow[3]
+			|| currWindow[1] === currWindow[2] || currWindow[1] === currWindow[3]
+			|| currWindow[2] === currWindow[3];
+	}
+	for(let index = 4; index < input.length; index++) {
+		console.log(currWindow);
+		if(!isRepeating()) {
+			displayCaption(`Found it at ${index}.`);
+			break;
+		}
+		currWindow.push(input[index]);
+		currWindow.shift();
 	}
 }
