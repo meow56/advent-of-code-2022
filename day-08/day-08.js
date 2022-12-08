739d1c69b99d5f878a0612a00d2eb404ae85fe9c
meow56 <spinattacktotheface@gmail.com>
2022-12-08 00:08:08 -0600

    Day 8-2
    Hahaha... don't you just love it when your code doesn't work because of a single new line character...?
    
    Also for some reason I got a different answer after that fix that was wrong. Don't ask, cuz I have no idea.

--- day-08/day-08.js
+++ day-08/day-08.js
@@ -8,6 +8,7 @@ function day8(input) {
 		trees.push(splitInput[i].split(""));
 		visibility.push(new Array(splitInput[i].length).fill(false));
 	}
+
 	function detVisibilityRow(row) {
 		let currHighest = -1;
 		for(let i = 0; i < trees[row].length; i++) {
@@ -57,6 +58,51 @@ function day8(input) {
 			return accu + tree;
 		}, 0);
 		return acc + rowVisible;
-	}, 0)
+	}, 0);
+
+	const MAX_ROW = trees.length;
+	const MAX_COLUMN = trees[0].length;
+
+	function moveRight(row, column, threshold) {
+		if(+trees[row][column] >= threshold) return 1;
+		if(column === MAX_COLUMN - 1) return 1;
+		return moveRight(row, column + 1, threshold) + 1;
+	}
+
+	function moveLeft(row, column, threshold) {
+		if(+trees[row][column] >= threshold) return 1;
+		if(column === 0) return 1;
+		return moveLeft(row, column - 1, threshold) + 1;
+	}
+
+	function moveUp(row, column, threshold) {
+		if(+trees[row][column] >= threshold) return 1;
+		if(row === 0) return 1;
+		return moveUp(row - 1, column, threshold) + 1;
+	}
+
+	function moveDown(row, column, threshold) {
+		if(+trees[row][column] >= threshold) return 1;
+		if(row === MAX_ROW - 1) return 1;
+		return moveDown(row + 1, column, threshold) + 1;
+	}
+
+	function calcScenicScore(row, column) {
+		if(row === 0 || column === 0 || row === MAX_ROW - 1 || column === MAX_COLUMN - 1) return 0;
+		const heightThreshold = +trees[row][column];
+		let leftValue = moveLeft(row, column - 1, heightThreshold);
+		let rightValue = moveRight(row, column + 1, heightThreshold);
+		let upValue = moveUp(row - 1, column, heightThreshold);
+		let downValue = moveDown(row + 1, column, heightThreshold);
+		return upValue * downValue * leftValue * rightValue;
+	}
+	let currMaxScore = 0;
+	for(let i = 0; i < MAX_ROW; i++) {
+		for(let j = 0; j < MAX_COLUMN; j++) {
+			currMaxScore = Math.max(currMaxScore, calcScenicScore(i, j));
+		}
+	}
+
 	displayCaption(`Number of visible trees: ${visibleCount}.`);
+	displayCaption(`Best score: ${currMaxScore}.`);
 }
