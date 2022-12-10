d1c07653be78f2cfea84f878bf5ad1781ba52edd
meow56 <spinattacktotheface@gmail.com>
2022-12-09 23:28:01 -0600

    Day 10-2
    Mmm. The only bug this time was incrementing cycle before doing stuff.
    
    Oh also, did you like my horrible regX joke? That's what I call funny.

--- day-10/day-10.js
+++ day-10/day-10.js
@@ -13,27 +13,36 @@ function day10(input) {
 		}
 	}
 
+	function duringCycle() {
+		if(cycles % 40 === 20) {
+			sum40By20 += regX * cycles;
+		}
+		let rowX = cycles % 40;
+		if(rowX === regX || rowX === regX + 1 || rowX === regX - 1) {
+			screen[cycles] = "█";
+		}
+	}
+
 	let cycles = 0;
 	let regX = 1;
 	let sum40By20 = 0;
+	let screen = new Array(240).fill(" ");
 	for(let i = 0; i < instructions.length; i++) {
 		if(instructions[i][0] === "noop") {
+			duringCycle();
 			cycles++;
-			if(cycles % 40 === 20) {
-				sum40By20 += regX * cycles;
-			}
 		} else {
+			duringCycle();
 			cycles++;
-			if(cycles % 40 === 20) {
-				sum40By20 += regX * cycles;
-			}
+			duringCycle();
 			cycles++;
-			if(cycles % 40 === 20) {
-				sum40By20 += regX * cycles;
-			}
 			regX += instructions[i][1];
 		}
 	}
 
 	displayCaption(`The sum is ${sum40By20}.`);
+	displayCaption(`Unfortunately, I cannot tell you what the screen says.`);
+	for(let i = 0; i < 6; i++) {
+		displayText(screen.slice(i * 40, (i + 1) * 40).join(""));
+	}
 }
