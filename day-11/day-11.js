fef99f3d269a29f786dbb4daaf27d4c070838e99
meow56 <spinattacktotheface@gmail.com>
2022-12-11 00:25:02 -0600

    Day 11-2
    You know the numbers get bad when BigInt throws a range error. I kinda wanted to create my own integer representation, but I remembered that addition and multiplication, uh... *mumble* *mumble* stays the same...? *mumble* over modulo.
    
    I just spent a bunch of time making it so you can see both parts and I totally made a bug in my inspect function lol.

--- day-11/day-11.js
+++ day-11/day-11.js
@@ -3,7 +3,9 @@
 function day11(input) {
 	const FILE_REGEX = /Monkey \d:\n(?:.+\n)+/g;
 	let monke = [];
+	let anxiousMonke = [];
 	let entry;
+	let finalMod = 1;
 	while(entry = FILE_REGEX.exec(input)) {
 		let ITEM_REGEX = /Starting items: (?:\d+,? ?)+/g;
 		let itemEntry = entry[0].match(ITEM_REGEX);
@@ -14,6 +16,7 @@ function day11(input) {
 		let TEST_REGEX = /Test: divisible by (\d+)/g;
 		let testEntry = [...entry[0].matchAll(TEST_REGEX)][0];
 		testEntry = +testEntry[1];
+		finalMod *= testEntry;
 		let TRUE_REGEX = /ue.+(\d)/g;
 		let trueEntry = [...entry[0].matchAll(TRUE_REGEX)][0];
 		trueEntry = +trueEntry[1];
@@ -21,19 +24,22 @@ function day11(input) {
 		let falseEntry = [...entry[0].matchAll(FALSE_REGEX)][0];
 		falseEntry = +falseEntry[1];
 		let newMonke = new Monke(itemEntry, opEntry, testEntry, trueEntry, falseEntry);
+		let otherMonke = new AnxietyMonke(itemEntry, opEntry, testEntry, trueEntry, falseEntry);
 		monke.push(newMonke);
+		anxiousMonke.push(otherMonke);
 	}
-
-	console.log("done!");
 
 	function Monke(items, operation, test, throwTrue, throwFalse) {
 		this.items = items;
+		for(let i = 0; i < this.items.length; i++) {
+			this.items[i] = +this.items[i];
+		}
 		if(operation[1] === "old") {
 			this.operation = old => old * old;
 		} else if(operation[0] === "*") {
 			this.operation = old => old * +operation[1];
 		} else {
-			this.operation = old => +old + +operation[1];
+			this.operation = old => old + +operation[1];
 		}
 		this.test = test;
 		this.throwTrue = throwTrue;
@@ -48,38 +54,80 @@ function day11(input) {
 
 		this.inspect = function() {
 			this.inspectCount++;
-			let inspected = items.shift();
-			console.groupCollapsed(`Monke inspect ${inspected}, ook!`);
+			let inspected = this.items.shift();
 			inspected = this.operation(inspected);
-			console.log(`Ook, you are now worried by ${inspected}!`);
 			inspected = Math.floor(inspected / 3);
-			console.log(`It's not broken, ook! Worry at ${inspected}!`);
 			if(inspected % this.test === 0) {
-				console.log(`I threw it to monke ${this.throwTrue}, okiki!!`);
 				monke[this.throwTrue].items.push(inspected);
 			} else {
-				console.log(`I threw it to monke ${this.throwFalse}, okiki!!`);
 				monke[this.throwFalse].items.push(inspected);
 			}
-			console.groupEnd();
 		}
 	}
 
-	function round(currRound) {
+	function AnxietyMonke(items, operation, test, throwTrue, throwFalse) {
+		this.items = items.slice();
+		for(let i = 0; i < this.items.length; i++) {
+			this.items[i] = +this.items[i];
+		}
+		if(operation[1] === "old") {
+			this.operation = old => old * old;
+		} else if(operation[0] === "*") {
+			this.operation = old => old * +operation[1];
+		} else {
+			this.operation = old => old + +operation[1];
+		}
+		this.test = test;
+		this.throwTrue = throwTrue;
+		this.throwFalse = throwFalse;
+		this.inspectCount = 0;
+
+		this.takeTurn = function() {
+			while(this.items.length !== 0) {
+				this.inspect();
+			}
+		}
+
+		this.inspect = function() {
+			this.inspectCount++;
+			let inspected = this.items.shift();
+			inspected = this.operation(inspected) % finalMod;
+			if(inspected % this.test === 0) {
+				anxiousMonke[this.throwTrue].items.push(inspected);
+			} else {
+				anxiousMonke[this.throwFalse].items.push(inspected);
+			}
+		}
+	}
+
+	function round() {
 		for(let i = 0; i < monke.length; i++) {
-			console.groupCollapsed(`Round ${currRound}, monke ${i}'s turn.`);
 			monke[i].takeTurn();
-			console.groupEnd();
+		}
+	}
+
+	function anxiousRound() {
+		for(let i = 0; i < anxiousMonke.length; i++) {
+			anxiousMonke[i].takeTurn();
 		}
 	}
 	for(let i = 0; i < 20; i++) {
-		round(i);
+		round();
+	}
+	for(let i = 0; i < 10000; i++) {
+		anxiousRound();
 	}
 
 	let monkeInspection = [];
+	let anxietyMonkeInspection = [];
 	for(let monk of monke) {
 		monkeInspection.push(monk.inspectCount);
 	}
+	for(let aaaa of anxiousMonke) {
+		anxietyMonkeInspection.push(aaaa.inspectCount);
+	}
 	monkeInspection.sort((a, b) => b - a);
+	anxietyMonkeInspection.sort((a, b) => b - a);
 	displayText(`The monkey business is ${monkeInspection[0] * monkeInspection[1]}.`);
+	displayText(`AAAAA!!! THE MONKEY BUSINESS IS ${anxietyMonkeInspection[0] * anxietyMonkeInspection[1]}!!!`);
 }
