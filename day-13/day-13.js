b54fc8c42c3b0696ee367ae46eac79be85a702b1
meow56 <spinattacktotheface@gmail.com>
2022-12-12 23:35:04 -0600

    Day 13-1
    Mwahahaha, I get to cheat by using Function()! Ahahahahaaaa!!!
    
    I'll probably try to implement an actual array parser later.

--- day-13/day-13.js
+++ day-13/day-13.js
@@ -1,8 +1,97 @@
 "use strict";
 
 function day13(input) {
-	const FILE_REGEX = /./g;
-	let entry;
-	while(entry = FILE_REGEX.exec(input)) {
+	let packets = input.split("\n");
+	let pairs = [[]];
+	for(let packet of packets) {
+		if(packet.length === 0) {
+			pairs.push([]);
+		} else {
+			let newArray = Function("return " + packet + ";");
+			pairs[pairs.length - 1].push(newArray());
+		}
 	}
+
+	function deepCopy(arr) {
+		let newArray = [];
+		for(let elem of arr) {
+			if(typeof elem === "number") {
+				newArray.push(elem);
+			} else {
+				newArray.push(deepCopy(elem));
+			}
+		}
+		return newArray;
+	}
+
+	function comparePairs(first, second) {
+		console.groupCollapsed(`Comparing ${first} and ${second}`);
+		first = deepCopy(first);
+		second = deepCopy(second);
+		while(first.length !== 0 && second.length !== 0) {
+			let firstElem = first.shift();
+			let secondElem = second.shift();
+			console.log(`Comparing elems ${firstElem} and ${secondElem}`);
+			if(typeof firstElem === "number" && typeof secondElem === "number") {
+				console.log(`Both elems are numbers.`);
+				if(firstElem > secondElem) {
+					console.log(`${firstElem} > ${secondElem}.`);
+					console.groupEnd();
+					return false;
+				} else if(secondElem > firstElem) {
+					console.log(`${firstElem} < ${secondElem}.`);
+					console.groupEnd();
+					return true;
+				}
+			} else if(typeof firstElem === "object" && typeof secondElem === "object") {
+				console.log(`Both elems are arrays.`);
+				let result = comparePairs(firstElem, secondElem);
+				if(result !== undefined) {
+					console.log(`Sub-lists ${result ? "were" : "were not"} sorted correctly.`);
+					console.groupEnd();
+					return result;
+				}
+			} else if(typeof firstElem === "number") {
+				console.log(`The first elem is a number.`);
+				let result = comparePairs([firstElem], secondElem);
+				if(result !== undefined) {
+					console.log(`Sub-lists ${result ? "were" : "were not"} sorted correctly.`);
+					console.groupEnd();
+					return result;
+				}
+			} else {
+				console.log(`The second elem is a number.`);
+				let result = comparePairs(firstElem, [secondElem]);
+				if(result !== undefined) {
+					console.log(`Sub-lists ${result ? "were" : "were not"} sorted correctly.`);
+					console.groupEnd();
+					return result;
+				}
+			}
+		}
+		if(first.length === second.length) {
+			console.log(`Conclusion: Equal.`);
+			console.groupEnd();
+			return undefined;
+		} else if(first.length === 0) {
+			console.log(`First array is shorter.`);
+			console.groupEnd();
+			return true;
+		} else {
+			console.log(`Second array is shorter.`);
+			console.groupEnd();
+			return false;
+		}
+	}
+
+	let sum = 0;
+	for(let i = 0; i < pairs.length; i++) {
+		if(pairs[i].length !== 0) {
+			let isSorted = comparePairs(pairs[i][0], pairs[i][1]);
+			if(isSorted) {
+				sum += i + 1;
+			}
+		}
+	}
+	displayCaption(`The correctly ordered pairs sum to ${sum}.`);
 }
