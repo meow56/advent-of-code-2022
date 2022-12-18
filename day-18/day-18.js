523f017a968b843a8980d35e7529b0d0f9d6502c
meow56 <spinattacktotheface@gmail.com>
2022-12-17 23:07:14 -0600

    Day 18-1
    Uh oh. This is too easy. I've got a bad feeling about part 2...

--- day-18/day-18.js
+++ day-18/day-18.js
@@ -1,8 +1,47 @@
 "use strict";
 
 function day18(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /(\d+),(\d+),(\d+)/g;
+	let cubes = [];
+	let cubeSet = new Set();
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
+		cubes.push(new Cube([+entry[1], +entry[2], +entry[3]]));
+		cubeSet.add(entry[0]);
 	}
+
+	function isFilled(pos) {
+		return cubeSet.has(pos.join());
+	}
+
+	function Cube(pos) {
+		this.pos = pos;
+
+		this.calcSurfaceArea = function() {
+			let exposedSides = 0;
+			let testPos = this.pos.slice();
+			testPos[0]--;
+			if(!isFilled(testPos)) exposedSides++;
+			testPos[0] += 2;
+			if(!isFilled(testPos)) exposedSides++;
+			testPos[0]--;
+			testPos[1]--;
+			if(!isFilled(testPos)) exposedSides++;
+			testPos[1] += 2;
+			if(!isFilled(testPos)) exposedSides++;
+			testPos[1]--;
+			testPos[2]--;
+			if(!isFilled(testPos)) exposedSides++;
+			testPos[2] += 2;
+			if(!isFilled(testPos)) exposedSides++;
+			testPos[2]--;
+			return exposedSides;
+		}
+	}
+
+	let totalSurfaceArea = 0;
+	for(let cube of cubes) {
+		totalSurfaceArea += cube.calcSurfaceArea();
+	}
+	displayCaption(`The total surface area is ${totalSurfaceArea}.`);
 }
