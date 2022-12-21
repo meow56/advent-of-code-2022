bfca2acd9fc08db152386de9fa235c55eacdee92
meow56 <spinattacktotheface@gmail.com>
2022-12-20 23:14:06 -0600

    Day 21-1
    Pretty quick. The monkeys are no longer monkeying around, so their array is now named monkeys. I feel like it would've been much easier to use Function instead of a switch statement to deal with the operator.

--- day-21/day-21.js
+++ day-21/day-21.js
@@ -1,8 +1,65 @@
 "use strict";
 
 function day21(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /([a-z]{4}): (?:(\d+)|(?:([a-z]{4}) (\+|-|\*|\/) ([a-z]{4})))/g;
+	let monkeys = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
+		if(entry[2]) {
+			monkeys.push(new Monkey(entry[1], +entry[2]));
+		} else {
+			monkeys.push(new Monkey(entry[1], [entry[3], entry[4], entry[5]]));
+		}
 	}
+
+	function findMonkey(name) {
+		for(let monkey of monkeys) {
+			if(monkey.name === name) return monkey;
+		}
+	}
+
+	for(let monkey of monkeys) {
+		monkey.initYell();
+	}
+
+	function Monkey(name, yell) {
+		this.name = name;
+		this.yell = yell;
+
+		this.initYell = function() {
+			if(typeof this.yell === "number") {
+				this.yellFunc = () => this.yell;
+				return;
+			}
+			this.firstMonkey = findMonkey(this.yell[0]);
+			this.secondMonkey = findMonkey(this.yell[2]);
+			switch(this.yell[1]) {
+			case "+":
+				this.yellFunc = function() {
+					return this.firstMonkey.yellFunc() + this.secondMonkey.yellFunc();
+				}.bind(this);
+				break;
+			case "-":
+				this.yellFunc = function() {
+					return this.firstMonkey.yellFunc() - this.secondMonkey.yellFunc();
+				}.bind(this);
+				break;
+			case "*":
+				this.yellFunc = function() {
+					return this.firstMonkey.yellFunc() * this.secondMonkey.yellFunc();
+				}.bind(this);
+				break;
+			case "/":
+				this.yellFunc = function() {
+					return this.firstMonkey.yellFunc() / this.secondMonkey.yellFunc();
+				}.bind(this);
+				break;
+			}
+		}
+	}
+
+
+	let root = findMonkey("root");
+	let result = root.yellFunc();
+	displayCaption(`The root monkey yells ${result}.`);
 }
