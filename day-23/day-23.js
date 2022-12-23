bcb95a85d3f88381faa75822925bccb696404d18
meow56 <spinattacktotheface@gmail.com>
2022-12-23 01:16:35 -0600

    Day 23-2
    I was right. How much time did this save? Every round takes ~1 second, so you're looking at probably 16 minutes of waiting. Ain't that nice?
    
    Also, you know what I like about template literals? You can insert new line characters with a literal new line. Leads to funny looking code.

--- day-23/day-23.js
+++ day-23/day-23.js
@@ -25,20 +25,41 @@ function day23(input) {
 			[[-1,  1], [ 0,  1], [ 1,  1]], // S
 			[[-1, -1], [-1,  0], [-1,  1]], // W
 			[[ 1, -1], [ 1,  0], [ 1,  1]]];// E
+		this.adjacency = [
+			[-1, -1],
+			[-1,  0],
+			[-1,  1],
+			[ 0, -1],
+			[ 0,  1],
+			[ 1, -1],
+			[ 1,  0],
+			[ 1,  1]];
+
+		this.finished;
 
 		this.isElfHere = function(pos) {
-			return this.elves.some((val) => val.pos[0] === pos[0] && val.pos[1] === pos[1]);
+			return this.elves.some(elf => elf.pos[0] === pos[0] && elf.pos[1] === pos[1]);
 		}
 
 		this.round = function(roundNum) {
 			console.log(`Beginning round ${roundNum}.`);
 			this.proposed.clear();
 			this.invalid.clear();
+
+			let willMove = false;
 			for(let elf of this.elves) {
-				elf.proposeMove();
+				if(!this.adjacency.every(function(offset) {
+					return !this.isElfHere([elf.pos[0] + offset[0], elf.pos[1] + offset[1]]);
+				}.bind(this))) {
+					willMove = true;
+					elf.proposeMove();
+				}
+			}
+			if(!willMove) {
+				return true;
 			}
 
-			console.log(`Proposals finished.`)
+			console.log(`Proposals finished.`);
 
 			for(let [pos, elf] of this.proposed) {
 				if(!this.invalid.has(pos)) {
@@ -53,10 +74,35 @@ function day23(input) {
 			this.moveOrder.push(rotation);
 
 			console.log(`Movement finished.`);
+
+			return false;
 		}
 
 		this.display = function() {
-			let minX, minY, maxX, maxY;
+			let minX = Number.MAX_SAFE_INTEGER;
+			let minY = Number.MAX_SAFE_INTEGER;
+			let maxX = Number.MIN_SAFE_INTEGER;
+			let maxY = Number.MIN_SAFE_INTEGER;
+			for(let elf of trueGrid.elves) {
+				if(elf.pos[0] < minX) minX = elf.pos[0];
+				if(elf.pos[1] < minY) minY = elf.pos[1];
+				if(elf.pos[0] > maxX) maxX = elf.pos[0];
+				if(elf.pos[1] > maxY) maxY = elf.pos[1];
+			}
+			let finalDisplay = `(${minX}, ${minY}) to (${maxX}, ${maxY}):
+`;
+			for(let y = minY; y <= maxY; y++) {
+				let row = "";
+				for(let x = minX; x <= maxX; x++) {
+					if(this.isElfHere([x, y])) {
+						row += "#";
+					} else {
+						row += ".";
+					}
+				}
+				finalDisplay += row + "\n";
+			}
+			return finalDisplay;
 		}
 	}
 
@@ -70,20 +116,13 @@ function day23(input) {
 			do {
 				moveDirInd++;
 				isValidMove = true;
-				let newPos;
-				if(moveDirInd === this.grid.moveOrder.length) break;
-				for(let dirs of this.grid.moveOrder[moveDirInd]) {
-					newPos = this.pos.slice();
-					newPos[0] += dirs[0];
-					newPos[1] += dirs[1];
-					if(this.grid.isElfHere(newPos)) isValidMove = false;	
+				if(moveDirInd === this.grid.moveOrder.length) return; // No moving allowed.
+				for(let dir of this.grid.moveOrder[moveDirInd]) {
+					if(this.grid.isElfHere([this.pos[0] + dir[0], this.pos[1] + dir[1]])) isValidMove = false;	
 				}
 			} while(!isValidMove);
-			if(moveDirInd >= this.grid.moveOrder.length) return; // No moving allowed.
-			let finalMove = this.pos.slice();
 			let trueVector = this.grid.moveOrder[moveDirInd][1];
-			finalMove[0] += trueVector[0];
-			finalMove[1] += trueVector[1];
+			let finalMove = [this.pos[0] + trueVector[0], this.pos[1] + trueVector[1]];
 			if(this.grid.proposed.has(finalMove.join())) {
 				// Someone else proposed it :(
 				this.grid.invalid.add(finalMove.join());
@@ -97,19 +136,26 @@ function day23(input) {
 		}
 	}
 
-	for(let i = 1; i <= 10; i++) {
-		trueGrid.round(i);
-	}
-	let minX = Number.MAX_SAFE_INTEGER;
-	let minY = Number.MAX_SAFE_INTEGER;
-	let maxX = Number.MIN_SAFE_INTEGER;
-	let maxY = Number.MIN_SAFE_INTEGER;
-	for(let elf of trueGrid.elves) {
-		if(elf.pos[0] < minX) minX = elf.pos[0];
-		if(elf.pos[1] < minY) minY = elf.pos[1];
-		if(elf.pos[0] > maxX) maxX = elf.pos[0];
-		if(elf.pos[1] > maxY) maxY = elf.pos[1];
+	console.log(trueGrid.display());
+
+	let roundNum = 1;
+	while(!trueGrid.round(roundNum)) {
+		if(roundNum === 10) {
+			let minX = Number.MAX_SAFE_INTEGER;
+			let minY = Number.MAX_SAFE_INTEGER;
+			let maxX = Number.MIN_SAFE_INTEGER;
+			let maxY = Number.MIN_SAFE_INTEGER;
+			for(let elf of trueGrid.elves) {
+				if(elf.pos[0] < minX) minX = elf.pos[0];
+				if(elf.pos[1] < minY) minY = elf.pos[1];
+				if(elf.pos[0] > maxX) maxX = elf.pos[0];
+				if(elf.pos[1] > maxY) maxY = elf.pos[1];
+			}
+
+			displayCaption(`The area without elves is ${(maxX - minX + 1) * (maxY - minY + 1) - trueGrid.elves.length}.`);
+		}
+		roundNum++;
 	}
 
-	displayCaption(`The area without elves is ${(maxX - minX + 1) * (maxY - minY + 1) - trueGrid.elves.length}.`);
+	displayCaption(`All in all, it took ${roundNum} rounds.`);
 }
