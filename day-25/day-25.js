d27f903100ea0de3b6b94b36a1943d12926f9b49
meow56 <spinattacktotheface@gmail.com>
2022-12-24 23:31:54 -0600

    Day 25-1
    End of the line. You can see I didn't bother with figuring out base 10 to centered base 5, so I just implemented centered base 5 add instead. At least parseNum was useful for debugging. One bug I ran into was forgetting to add the carry at the end, so I got an integer overflow. Quite the fitting end, I think.

--- day-25/day-25.js
+++ day-25/day-25.js
@@ -1,8 +1,80 @@
 "use strict";
 
 function day25(input) {
-	const FILE_REGEX = /./g;
-	let entry;
-	while(entry = FILE_REGEX.exec(input)) {
+	let numbers = input.split("\n");
+
+	function parseNum(number) {
+		let digits = number.split("");
+		let place = 1;
+		let sum = 0;
+		for(let i = digits.length - 1; i >= 0; i--) {
+			if(!Number.isNaN(+digits[i])) {
+				sum += +digits[i] * place;
+			} else if(digits[i] === "-") {
+				sum += -place;
+			} else {
+				sum += -2 * place;
+			}
+			place *= 5;
+		}
+		return sum;
+	}
+
+	let totalSum = "0";
+	for(let num of numbers) {
+		totalSum = add(totalSum, num);
+	}
+	displayCaption(`The sum is ${totalSum} (${parseNum(totalSum)}).`);
+	function reverseParse(number) {
+
+	}
+
+	function add(num1, num2) {
+		let maxLength = Math.max(num1.length, num2.length);
+		num1 = num1.padStart(maxLength, "0");
+		num2 = num2.padStart(maxLength, "0");
+		let num1Arr = num1.split("");
+		num1Arr = num1Arr.map(digit => {
+			if(digit === "-") return -1;
+			if(digit === "=") return -2;
+			return +digit;
+		});
+		let num2Arr = num2.split("");
+		num2Arr = num2Arr.map(digit => {
+			if(digit === "-") return -1;
+			if(digit === "=") return -2;
+			return +digit;
+		});
+		let finalSum = [];
+		let carry = 0;
+		for(let i = maxLength - 1; i >= 0; i--) {
+			let digit1 = num1Arr[i];
+			let digit2 = num2Arr[i];
+			let digitSum = digit1 + digit2 + carry;
+			if(digitSum > 2) {
+				carry = 0;
+				while(digitSum > 2) {
+					digitSum -= 5;
+					carry++;
+				}
+			} else if(digitSum < -2) {
+				carry = 0;
+				while(digitSum < -2) {
+					digitSum += 5;
+					carry--;
+				}
+			} else {
+				carry = 0;
+			}
+			finalSum.unshift(digitSum);
+		}
+		if(carry !== 0) finalSum.unshift(carry);
+		let trueFinale = "";
+		for(let digit of finalSum) {
+			if(digit >= 0) trueFinale += digit;
+			else if(digit === -1) trueFinale += "-";
+			else trueFinale += "=";
+		}
+		return trueFinale;
 	}
 }
